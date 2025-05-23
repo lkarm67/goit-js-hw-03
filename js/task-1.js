ac1d513f68c98b44ae9888f0f182953cb5f64543
Luidmyla Karmeliuk <lkarm67@gmail.com>
2025-05-23 11:03:10 +0300

    fix js, html

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,8 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) { 
-  const totalPrice = quantity * pricePerDroid;
-  if (totalPrice <= customerCredits) {
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`
-  } else {
-    return "Insufficient funds!"
-  }  
+function slugify(title) {
+  const words = title.split(" ");
+  return words.join('-').toLowerCase();
 }
-
-console.log(makeTransaction(5, 3000, 23000));
-console.log(makeTransaction(3, 1000, 15000));
-console.log(makeTransaction(10, 5000, 8000));
-console.log(makeTransaction(8, 2000, 10000));
-console.log(makeTransaction(10, 500, 5000));
-
-
+console.log(slugify("Arrays for beginners"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,16 @@
-function checkForSpam(message) {
-  if (message.toLowerCase().includes("sale") || message.toLowerCase().includes("spam")) { 
-    return true;
-  } else {
-    return false;
+function filterArray(numbers, value) {
+  const numbersHigherValue = [];
+  for (let i = 0; i < numbers.length; i++) {
+    
+    if (numbers[i] > value) {
+      numbersHigherValue.push(numbers[i]);
+    }
   }
+  return numbersHigherValue;
 }
 
-console.log(checkForSpam("Latest technology news"));
-console.log(checkForSpam("JavaScript weekly newsletter"));
-console.log(checkForSpam("Get best sale offers now!"));
-console.log(checkForSpam("Amazing SalE, only tonight!"));
-console.log(checkForSpam("Trust me, this is not a spam message"));
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
-console.log(checkForSpam("[SPAM] How to earn fast money?"));
-
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
